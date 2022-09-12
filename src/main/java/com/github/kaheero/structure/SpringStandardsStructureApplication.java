e1dd802d91bebe05eac12c1a585755ae5ab7269d
Kairo Rodrigues dos Santos <kairo.santos@g.globo>
2022-09-12 12:36:21 -0300

    initial commit

---
+++ src/main/java/com/github/kaheero/structure/SpringStandardsStructureApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaheero.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStandardsStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStandardsStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kaheero/structure/SpringStandardsStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kaheero.structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStandardsStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
